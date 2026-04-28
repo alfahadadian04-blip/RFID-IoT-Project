96fbb23195bda6a383c9b0c29ee947397ec26801
ViktorGreg <limviktorgreg14@gmail.com>
2026-04-29 06:30:25 +0800

    Added Static on settings.py for deployment

--- CCS_RFID/CCS_RFID/settings.py
+++ CCS_RFID/CCS_RFID/settings.py
@@ -108,7 +108,11 @@ USE_TZ = False  # CHANGE THIS TO False - THIS IS THE KEY FIX
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 # Media files (Profile pictures, uploaded files)
 MEDIA_URL = '/media/'
